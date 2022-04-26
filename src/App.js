824c432f4a2db807875289162bbe924344e5639a
Paris Dafermos <parisdafermos@gmail.com>
2022-04-26 13:01:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {
+  AppBar,
+  Card,
+  CardContent,
+  Grid,
+  Link,
+  TextField,
+  Toolbar,
+  Typography,
+  Box,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("./data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+        console.log(data.locations[0].consumers[0]);
+        //console logs for testing purposes
+        const locationConsumers = data.locations.map((location) => {
+          return {
+            consumers: location.consumers,
+          };
+        });
+        const individualConsumer = locationConsumers.map((consumer) => {
+          console.log({
+            name: consumer.name,
+            // I get undefined here
+          });
+        });
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Search consumer</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box mt={1}>
+        <Grid
+          alignItems="center"
+          container
+          direction="column"
+          justify="center"
+          spacing={3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid item md={6} sm={9} xs={12}>
+            <TextField label="Search" />
+          </Grid>
+
+          <Grid item md={6} sm={9} xs={12}>
+            <Card>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  2020-05-22(Occupation date)
+                  {/* hardcoded currently */}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  FirstName LastName(name)
+                  {/* hardcoded currently */}
+                </Typography>
+                <Typography color="textSecondary" gutterBottom>
+                  <Link href="http://google.com" rel="noopener" target="_blank">
+                    http://test12@gmail.com(email)
+                    {/* hardcoded currently */}
+                  </Link>
+                </Typography>
+                <Typography variant="h6" component="h2">
+                  07546983574(phone)
+                  {/* hardcoded currently */}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 }
 
